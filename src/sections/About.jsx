fb2529e8ea5be9ac4e4836459177300b759c6875
nitin-suryapally <nitin28suryapally@gmail.com>
2024-10-27 14:39:43 +0530

    (feat): chnaged location update

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -63,7 +63,7 @@ const About = () => {
             </div>
             <div>
               <p className="grid-headtext">I’m very flexible with time zone communications & locations</p>
-              <p className="grid-subtext">I&apos;m based in Rjieka, Croatia and open to remote work worldwide.</p>
+              <p className="grid-subtext">I&apos;m based in Hyderabad, India and open to remote work worldwide.</p>
               <Button name="Contact Me" isBeam containerClass="w-full mt-10" />
             </div>
           </div>
